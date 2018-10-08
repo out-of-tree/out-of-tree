e7d1e2f327107a6b218b7130b598091999e18bc2
Mikhail Klementev <jollheef@riseup.net>
2018-10-08 20:51:32 +0000

    Change license to GNU AGPLv3

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 // Copyright 2018 Mikhail Klementev. All rights reserved.
-// Use of this source code is governed by a GPLv3 license
+// Use of this source code is governed by a AGPLv3 license
 // (or later) that can be found in the LICENSE file.
 
 package main
