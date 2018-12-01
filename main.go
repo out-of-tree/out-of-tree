b36956c7a443c2fbc9b3c2be2cac439ee4762223
Mikhail Klementev <jollheef@riseup.net>
2018-12-01 18:12:44 +0000

    Bump version in source code

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 	)
 
 	app.Author("Mikhail Klementev <jollheef@riseup.net>")
-	app.Version("0.1.0")
+	app.Version("0.2.0")
 
 	pathFlag := app.Flag("path", "Path to work directory")
 	path := pathFlag.Default(".").ExistingDir()
