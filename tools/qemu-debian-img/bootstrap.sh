bfae451749ac25255798d2e49ad5c43b43bf1246
Mikhail Klementev <blame@dumpstack.io>
2020-05-30 12:31:27 +0000

    Fix kernel module name for unit tests

--- tools/qemu-debian-img/bootstrap.sh
+++ tools/qemu-debian-img/bootstrap.sh
@@ -6,4 +6,4 @@ docker run --privileged -v $(pwd):/shared -t gen-ubuntu2004-image
 RUN="docker run -v $(pwd):/shared -t gen-ubuntu2004-image"
 $RUN sh -c 'chmod 644 /boot/vmlinuz && cp /boot/vmlinuz /shared/ubuntu2004.vmlinuz'
 $RUN sh -c 'cp /boot/initrd.img /shared/ubuntu2004.initrd'
-$RUN sh -c 'cp $(find /lib/modules -name test_static_key_base.ko) /shared/ubuntu2004.ko'
+$RUN sh -c 'cp $(find /lib/modules -name test_bpf.ko) /shared/ubuntu2004.ko'
