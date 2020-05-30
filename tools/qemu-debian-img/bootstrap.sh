9b8d4a056e1ff1cd055e96e5e15f6fb67155cbe9
Mikhail Klementev <blame@dumpstack.io>
2020-05-30 12:05:24 +0000

    Fix path to vmlinuz/initrd

--- tools/qemu-debian-img/bootstrap.sh
+++ tools/qemu-debian-img/bootstrap.sh
@@ -4,6 +4,6 @@ cd $(dirname $(realpath $0))
 docker build -t gen-ubuntu2004-image .
 docker run --privileged -v $(pwd):/shared -t gen-ubuntu2004-image
 RUN="docker run -v $(pwd):/shared -t gen-ubuntu2004-image"
-$RUN sh -c 'chmod 644 /vmlinuz && cp /vmlinuz /shared/ubuntu2004.vmlinuz'
-$RUN sh -c 'cp /initrd.img /shared/ubuntu2004.initrd'
+$RUN sh -c 'chmod 644 /boot/vmlinuz && cp /boot/vmlinuz /shared/ubuntu2004.vmlinuz'
+$RUN sh -c 'cp /boot/initrd.img /shared/ubuntu2004.initrd'
 $RUN sh -c 'cp $(find /lib/modules -name test_static_key_base.ko) /shared/ubuntu2004.ko'
