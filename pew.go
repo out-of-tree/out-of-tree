8ec4f13364679004bbfe90697decfdb8fdd68f81
Mikhail Klementev <blame@dumpstack.io>
2023-05-08 19:18:49 +0000

    Lower log level for debug output

--- pew.go
+++ pew.go
@@ -483,7 +483,7 @@ func copyArtifactAndTest(slog zerolog.Logger, q *qemu.System, ka config.Artifact
 			slog.Error().Err(err).Msg(res.Test.Output)
 			return
 		}
-		slog.Info().Msg(res.Test.Output)
+		slog.Debug().Msg(res.Test.Output)
 		res.Run.Ok = true
 		res.Test.Ok = true
 	default:
