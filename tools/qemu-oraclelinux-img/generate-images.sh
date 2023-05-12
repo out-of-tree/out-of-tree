8f06448240201361e06a266c37fb26c428550648
Mikhail Klementev <blame@dumpstack.io>
2023-05-12 08:11:57 +0000

    Switch to podman

--- tools/qemu-oraclelinux-img/generate-images.sh
+++ tools/qemu-oraclelinux-img/generate-images.sh
@@ -2,17 +2,24 @@
 
 set -eux
 
+cd $(dirname $(realpath $0))
+
 for version in 6 7 8 9; do
     mkdir $version
+
     sed "s/_VERSION_/${version}/" Dockerfile.template >> $version/Dockerfile
+
     if [[ $version -eq 6 ]]; then
 	sed -i 's/baseos_latest/u10_base/' $version/Dockerfile
     fi
     if [[ $version -eq 7 ]]; then
 	sed -i 's/baseos_latest/u9_base/' $version/Dockerfile
     fi
-    docker build -t gen-oraclelinux${version}-image $version
+
+    podman build -t gen-oraclelinux${version}-image $version
     rm -rf $version
-    docker run --privileged -v $(pwd):/shared -t gen-oraclelinux${version}-image
+
+    podman run --privileged -v $(pwd):/shared -t gen-oraclelinux${version}-image
+
     tar -Szcf out_of_tree_oraclelinux_${version}.img.tar.gz out_of_tree_oraclelinux_${version}.img
 done
