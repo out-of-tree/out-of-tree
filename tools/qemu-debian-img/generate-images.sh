310999744e7a76e6d3e3f490f2ac656bf0451797
Mikhail Klementev <blame@dumpstack.io>
2023-05-12 02:05:36 +0000

    Untabify

--- tools/qemu-debian-img/generate-images.sh
+++ tools/qemu-debian-img/generate-images.sh
@@ -6,24 +6,24 @@ cd $(dirname $(realpath $0))
 
 for version in 7 8 9 10 11; do
     if [[ $version -eq 7 ]]; then
-	release=wheezy
-	last_version=7.11.0
+        release=wheezy
+        last_version=7.11.0
     fi
     if [[ $version -eq 8 ]]; then
-	release=jessie
-	last_version=8.11.1
+        release=jessie
+        last_version=8.11.1
     fi
     if [[ $version -eq 9 ]]; then
-	release=stretch
-	last_version=9.13.0
+        release=stretch
+        last_version=9.13.0
     fi
     if [[ $version -eq 10 ]]; then
-	release=buster
-	last_version=10.13.0
+        release=buster
+        last_version=10.13.0
     fi
     if [[ $version -eq 11 ]]; then
-	release=bullseye
-	last_version=11.6.0
+        release=bullseye
+        last_version=11.6.0
     fi
 
     mkdir $version
@@ -32,7 +32,7 @@ for version in 7 8 9 10 11; do
     sed -i "s/_RELEASE_/${release}/" $version/Dockerfile
 
     if [[ $version -eq 11 ]]; then
-	sed -i "s/,policykit-1//" $version/Dockerfile
+        sed -i "s/,policykit-1//" $version/Dockerfile
     fi
 
     repository=$(wget -q -O - https://cdimage.debian.org/mirror/cdimage/archive/${last_version}/amd64/jigdo-bd/debian-${last_version}-amd64-BD-1.jigdo | gunzip | awk -F= '/snapshot.debian.org/ {print $2}' | cut -d ' ' -f 1)
