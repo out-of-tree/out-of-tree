2daa11119685ff4e892d28ded374b82c33647697
Mikhail Klementev <root@dumpstack.io>
2024-10-17 17:55:23 +0000

    feat(qemu images): generate all ubuntu images

--- tools/qemu-ubuntu-img/generate-images.py
+++ tools/qemu-ubuntu-img/generate-images.py
@@ -7,13 +7,13 @@ script_dir = os.path.dirname(os.path.realpath(__file__))
 os.chdir(script_dir)
 
 releases = [
-    # ('12.04', 'precies'),
-    # ('14.04', 'trusty'),
-    # ('16.04', 'xenial'),
-    # ('18.04', 'bionic'),
-    # ('20.04', 'focal'),
-    # ('22.04', 'jammy'),
-    ('24.04', 'noble'),
+    ('12.04', 'precise', 'http://old-releases.ubuntu.com/ubuntu'),
+    ('14.04', 'trusty', 'http://archive.ubuntu.com/ubuntu'),
+    ('16.04', 'xenial', 'http://archive.ubuntu.com/ubuntu'),
+    ('18.04', 'bionic', 'http://archive.ubuntu.com/ubuntu'),
+    ('20.04', 'focal', 'http://archive.ubuntu.com/ubuntu'),
+    ('22.04', 'jammy', 'http://archive.ubuntu.com/ubuntu'),
+    ('24.04', 'noble', 'http://archive.ubuntu.com/ubuntu')
 ]
 
 template = '''
@@ -27,7 +27,7 @@ RUN apt install -y linux-image-generic
 ENV TMPDIR=/tmp/ubuntu
 ENV IMAGEDIR=/tmp/image
 ENV IMAGE=/shared/out_of_tree_ubuntu_{img_version}.img
-ENV REPOSITORY=http://archive.ubuntu.com/ubuntu
+ENV REPOSITORY={repository}
 ENV RELEASE={codename}
 
 RUN mkdir $IMAGEDIR
@@ -47,7 +47,7 @@ def run_cmd(cmd):
     print(f"+ {cmd}")
     subprocess.run(cmd, shell=True, check=True, executable='/bin/bash')
 
-for version, codename in releases:
+for version, codename, repository in releases:
     numeric_version = version.replace('.', '')
     img_version=version.replace(".","__")
 
@@ -55,6 +55,7 @@ for version, codename in releases:
         version=version,
         img_version=img_version,
         codename=codename,
+        repository=repository,
         numeric_version=numeric_version)
 
     os.makedirs(str(version), exist_ok=True)
